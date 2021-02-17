7d1349d2d6a5199ab7a7508ef9fa4e2d7f9d9092
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2021-02-17 19:25:14 +0200

    Test graph pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TEST GRAPH PAGES
         </a>
       </header>
     </div>
